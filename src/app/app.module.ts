d2d3258933838b4f6d28600c59359ed681010185
Tulio Ferreira <tuliof210@gmail.com>
2021-02-10 01:45:44 -0300

    bugfix route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app.routes';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackGroundComponent } from './common/back-ground/back-ground.component';
 import { HomeComponent } from './views/home/home.component';
