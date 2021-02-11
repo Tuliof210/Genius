018443eb98356135fce4f0d9e515faa9ce4b7f93
Tulio Ferreira <tuliof210@gmail.com>
2021-02-10 23:02:21 -0300

    set logo

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  readonly logoAlt = 'JS G3niu5';
+  readonly logoDirecoty = '../../../assets/images/logo.svg';
+
   constructor() {}
 
   ngOnInit(): void {}
