071615150b2d0da59b03f38dd0580334670fe2c5
Tulio Ferreira <tuliof210@gmail.com>
2021-02-14 22:41:00 -0300

    mock play

--- src/app/views/in-game/in-game.component.ts
+++ src/app/views/in-game/in-game.component.ts
@@ -6,7 +6,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./in-game.component.scss'],
 })
 export class InGameComponent implements OnInit {
+  countDown;
+
+  currentScore = 0;
+  order = [];
+  display = 0;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.start();
+  }
+
+  start() {
+    setTimeout(() => {
+      this.newRound();
+    }, 2000);
+  }
+
+  newRound() {
+    this.order.push(Math.floor(Math.random() * 10));
+    this.displayValue(0, this.order.length);
+  }
+
+  displayValue(index, max) {
+    this.display = this.order[index];
+    index++;
+    if (index === max) this.simulatePlayer();
+    else
+      setTimeout(() => {
+        this.displayValue(index, max);
+      }, 300);
+  }
+
+  simulatePlayer() {
+    let score = true;
+    for (let num of this.order) {
+      if (this.play(num)) continue;
+      else {
+        score = false;
+        break;
+      }
+    }
+    if (score) {
+      this.currentScore += 1;
+      this.newRound();
+    } else
+      console.log('Finish', this.order, {
+        finalScore: this.currentScore,
+      });
+  }
+
+  play(num) {
+    const playerTry = Math.floor(Math.random() * 10);
+    console.log({ playerTry });
+    return num === playerTry;
+  }
 }
